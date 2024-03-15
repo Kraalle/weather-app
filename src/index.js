9671154a03df9a9b5b8d2b916c6627c9272db1fa
Max McLaren <maxtova@yahoo.com>
2024-03-14 20:33:45 -0700

    added getWeather function that retrieves all weather data from specified city

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("testing");
+async function getWeather() {
+  const response = await fetch(
+    "https://api.weatherapi.com/v1/current.json?key=33417db233ee48d59c313733240603&q=london",
+    { mode: "cors" }
+  );
+  response.json().then(function (response) {
+    console.log(response);
+  });
+}
+
+getWeather();
